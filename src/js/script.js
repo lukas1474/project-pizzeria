3c3e58563edc73ca9a43c2d2595d11b3a4b04543
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-05-01 22:33:42 +0200

    add renderInMenu(){} in class Product{}

--- src/js/script.js
+++ src/js/script.js
@@ -53,10 +53,31 @@
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      //console.log(generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
@@ -69,9 +90,6 @@
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
-
-      /* const testProduct = new Product();
-       console.log('testProduckt:', testProduct);*/
     },
 
     initData: function () {
