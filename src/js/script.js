77b23f4fe68429010dd9ea46e778780436d9f5f3
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-05-11 19:31:06 +0200

    finish addToCart(){}

--- src/js/script.js
+++ src/js/script.js
@@ -188,6 +188,7 @@
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCard();
       });
     }
 
@@ -198,7 +199,9 @@
       /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
       /* przeczytaj wszystkie dane z formularza (using utils.serializeFormToObject) i zapisz to w stałej formData */
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
+
+      thisProduct.params = {};
 
       /* set variable price to equal thisProduct.data.price */
       /* ustaw zmienną cenę na równą thisProduct.data.price */
@@ -250,6 +253,14 @@
           const selectedImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
 
           if (optionSelected) {
+            if(!thisProduct.params[paramId]){
+
+              thisProduct.params[paramId] = {
+                label: param.label,
+                options: {},
+              };
+            }
+            thisProduct.params[paramId].options[optionId] = option.label;
 
             for (let selectedImage of selectedImages) {
               selectedImage.classList.add(classNames.menuProduct.imageVisible);
@@ -271,14 +282,19 @@
 
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       /* ustaw zawartości thisProduct.priceElem jako wartość ceny zmiennej */
-      price *= thisProduct.amountWidget.value;
+      //price *= thisProduct.amountWidget.value;
+      thisProduct.priceSingle = price;
+      thisProduct.price = thisProduct.priceSingle * thisProduct.amountWidget.value;
 
-      thisProduct.price = price;
+
+      //thisProduct.price = price;
 
       /* multiply price by amount */
       /* pomnóż cenę przez kwotę */
 
       thisProduct.priceElem.innerHTML = thisProduct.price;
+
+      console.log(thisProduct.params);
 
     }
 
@@ -290,6 +306,15 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
+    }
+
+    addToCard(){
+      const thisProduct = this;
+
+      thisProduct.name = thisProduct.data.name;
+      thisProduct.amount = thisProduct.amountWidget.value;
+
+      app.cart.add(thisProduct);
     }
 
   }
@@ -382,6 +407,8 @@
       thisCart.dom.wrapper = element;
 
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      thisCart.dom.productList = document.querySelector(select.containerOf.cart);
+
     }
 
     initActions (){
@@ -391,6 +418,20 @@
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
+    }
+
+    add(menuProduct){
+      const thisCart = this;
+
+      const generatedHTML = templates.cartProduct(menuProduct);
+      console.log(generatedHTML);
+
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      console.log(generatedDOM);
+
+      thisCart.dom.productList.appendChild(generatedDOM);
+
+      console.log('adding product', menuProduct);
     }
 
   }
